cd8a766359a9e401df9d930969242dfeb9119820
akke-t <34750068+akke-t@users.noreply.github.com>
2018-08-28 20:35:50 +0200

    Included explicit examples + restoration command.

--- taskwarrior-backup.sh
+++ taskwarrior-backup.sh
@@ -3,3 +3,9 @@
 # Create an archive of the Taskwarrior $TASK_DIR
 # Backups are kept in $TASK_BACKUP_DIR
 cd $TASK_BACKUP_DIR && tar -czf task-backup-$(date +'%Y%m%d%H%M%S').tar.gz -C $TASK_DIR .
+
+# E.g.:
+#A. Backup with (substitute your own directories in the ones I list):
+#cd "/mnt/c/Users/a/Documents/fun/taskwarrior/task backup/18-08-28 Github backup method/backup_dir" && tar -czf task-backup-$(date +'%Y%m%d%H%M%S').tar.gz -C "/home/a/.task" .
+#B. Restore with: 
+#tar -xf "/mnt/c/Users/a/Documents/fun/taskwarrior/task backup/18-08-28 Github backup method/backup_dir/task-backup-20180828194105.tar.gz" -C "/home/a/.task"
